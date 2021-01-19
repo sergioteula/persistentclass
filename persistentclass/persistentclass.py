2c8d36c421576b3687015ec7e5bec087ab7190ec
Sergio Abad <sergio.abad@corp.arcatelecom.com>
2021-01-19 16:27:58 +0100

    Added optional dill and improved code

--- persistentclass/persistentclass.py
+++ persistentclass/persistentclass.py
@@ -1,44 +1,61 @@
 # -*- coding: utf-8 -*-
-import dill
+import pickle
 import os
 
 
 class Persistent:
-    """This class makes data permanent using object storage with dill"""
-    def __init__(self, data_id, autosave=True, folder=None):
+    """This class makes data permanent using object storage with pickle"""
+    def __init__(self, data_id='', folder='', autosave=True, use_dill=False):
         self.data_id = data_id
-        if not folder:
-            folder = './'
+        self.autosave = autosave
+        self.use_dill = use_dill
+
+        # Create forlders structure
+        if folder is None:
+            folder = ''
         elif not os.path.exists(folder):
             os.makedirs(folder)
-        self.file_name = folder + '/' + self.__class__.__name__ + str(self.data_id) + '.dpo'
-        self.autosave = autosave
-        if not self._load():
-            self._init()
+        self.folder = folder
+
+        # Create file name
+        self.filename = self.__class__.__name__ + str(self.data_id) + '.pickle'
+        self.path = self.folder + self.filename
+
+        # Initialize data if not loaded
+        if not self.load():
+            self.init()
             self.save()
 
-    def _load(self):
+    def load(self):
         """Loads the data."""
-        if os.path.isfile(self.file_name):
-            with open(self.file_name, 'rb') as f:
-                data = dill.load(f)
+        if os.path.isfile(self.path):
+            with open(self.path, 'rb') as f:
+                if self.use_dill:
+                    import dill
+                    data = dill.load(f)
+                else:
+                    data = pickle.load(f)
                 self.__dict__.update(data.__dict__)
             return True
         else:
-            f = open(self.file_name, 'xb')
-            f.close()
+            with open(self.file_name, 'xb'):
+                pass
             return False
 
     def save(self):
         """Saves the data."""
-        with open(self.file_name, 'wb') as f:
-            dill.dump(self, f)
+        with open(self.path, 'wb') as f:
+            if self.use_dill:
+                import dill
+                dill.dump(self, f)
+            else:
+                pickle.dump(self, f)
 
-    def save_check(self):
+    def asave(self):
         """Saves the data if autosave is True."""
         if self.autosave:
             self.save()
 
-    def _init(self):
+    def init(self):
         """Initializes the object. Replace this on your class with custom method."""
         pass
